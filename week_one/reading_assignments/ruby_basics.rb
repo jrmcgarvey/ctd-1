236fa6a36ce6701f2fed6d948183f51877f4aed2
Kristine <kteich88@gmail.com>
2018-09-22 12:56:06 -0400

    updated ruby_basics.rb

--- week_one/reading_assignments/ruby_basics.rb
+++ week_one/reading_assignments/ruby_basics.rb
@@ -3,21 +3,40 @@
 # please use your knowledge of variable assignments in each response
 # when you are done, share your code with your instructor, push it to Github, or submit a pull request
 
+# variables
+x = 27 # integer
+name = "Kristine" # string
+y = 27.0 # float
 
-# output an integer
-
-
-
-# output the number of seconds in a week
-
-
-
-# output a float
-
+# expressions - using variable assignments to make equations
+x = 20
+y = 7
+x + y
 
 
-# output a string
+# concatenation
+name = "Kristine"
+"Hello my name is " + name
+
+# string interpolation
+name = "Kristine"
+print "What is your name? "
+name = gets.chomp
+puts "Hello #{name}!"
+
+# loops
+command = ''
+while command != 'bye'
+  puts command
+  command = gets.chomp
+end
+puts 'Come again soon!'
 
 
+# arrays
+[]
+array = Array.new
+names = ['Kristine', 'Rene', 'Shu', 'Alik', 'Victor', 'John']
 
-# output concatination of strings
+names << 'Gracias'
+names.push 'Jennifer'
