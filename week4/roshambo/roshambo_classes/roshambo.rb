46f540fcca238ce57cf929f1775c8a29d8a38530
Kristine <kteich88@gmail.com>
2018-10-06 16:49:25 -0400

    implemented roshambo using methods and classes

---
+++ week4/roshambo/roshambo_classes/roshambo.rb
@@ -0,0 +1,57 @@
+require "pry"
+
+require_relative "game"
+
+class Roshambo < Game
+  attr_reader :choices, :round, :wins, :losses, :draws, :player_choice, :ai_choice
+  attr_writer :player_choice, :ai_choice, :wins, :losses, :draws # allows me to rewrite variables
+
+  def initialize(game)
+    @choices = %w(rock paper scissors)
+    @rounds = game.rounds
+    @wins = game.wins
+    @losses = game.losses
+    @draws = game.draws
+    @player_choice = ""
+    @ai_choice = ""
+  end
+
+  def self.play(roshambo)
+    roshambo.rounds.times do
+      player_choice = player_choice(roshambo)
+      roshambo.ai_choice = roshambo.choices[rand(3)]
+      round_results(roshambo)
+    end
+  end
+
+  def self.player_choice(roshambo)
+    until roshambo.choices.include?(roshambo.player_choice.downcase)
+      puts '<----------------- Rock, Paper, Scissors, Shoot: ----------------->'
+      roshambo.player_choice = gets.chomp.downcase
+    end
+  end
+
+  def self.round_results(roshambo)
+    puts "You chose #{roshambo.player_choice}, AI choose #{roshambo.ai_choice}"
+    draw(roshambo) if roshambo.player_choice == roshambo.ai_choice
+    loss(roshambo) if roshambo.player_choice == roshambo.choices[roshambo.choices.index(roshambo.ai_choice) - 1]
+    win(roshambo) if roshambo.player_choice == roshambo.choices[roshambo.choices.index(roshambo.ai_choice) - 2]
+  end
+
+
+  def self.win(roshambo)
+    puts "---->You win"
+    roshambo.wins += 1
+  end
+
+  def self.loss
+    puts "---->AI wins"
+    roshambo.losses += 1
+  end
+
+  def self.draw
+    puts "---->draw"
+    roshambo.draws += 1
+  end
+
+end
