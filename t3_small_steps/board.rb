d5ed6e8ca22366d00191f8a69df0554331f9c668
John McGarvey <jmcgarve@bellsouth.net>
2018-10-17 21:40:41 +0000

    Change to make the examples match Kristine's

--- t3_small_steps/board.rb
+++ t3_small_steps/board.rb
@@ -1,19 +1,29 @@
 class Board 
     def initialize
-        @board=[["_","_","_"],["_","_","_"],["_","_","_"]]
-        # OR
-        # @board=Array.new(3) { ["_","_","_"] }
-    end 
-    
+     # initialize board
+        # set up blank data structure
+        @board = [[nil, nil, nil], 
+                  [nil, nil, nil], 
+                  [nil, nil, nil]]
+    end
+    # render
     def render
+        # loop through data structure
         @board.each do |row|
-            row.each {|entry|  print entry }
+            row.each do |column|
+                # display an existing marker if any, else blank
+                if column.nil? # if the column value is empty 
+                    print "#" # print an underscore value to display the available spots on the board
+                else
+                    print column.to_s
+                end
+            end
             puts
         end
     end
-    
+
     def coordinates_available?(c)
-        return true if @board[c[1],c[0]]=="_"
+        return true if @board[c[1]][c[0]]==nil
     end
     
     def add_piece(piece,c)
@@ -25,7 +35,7 @@ class Board
     end
     
     def columns
-        cols=Array.new(3) { Array.new(3) {"_"}}
+        cols=Array.new(3) { Array.new(3)}
         for i in 0..2 do
             for j in 0..2 do
                 cols[i][j]=@board[j][i]
@@ -35,19 +45,12 @@ class Board
     end
     
     def diagonals
-        diags=Array.new(2) { Array.new(3) {"_"}}
-        diags[0][0]=@board[0][0]
-        diags[0][1]=@board[1][1]
-        diags[0][2]=@board[2][2]
-        diags[1][0]=@board[0][2]
-        diags[1][1]=@board[1][1]
-        diags[1][2]=@board[2][0]
-        return diags
+        return [[@board[0][0],@board[1][1],@board[2][2]],[@board[0][2],@board[1][1],@board[2][0]]]
     end
     
     def find_winner(my_array)
         my_array.each do |row|
-            return true if row[0] != "_" && row[0] == row[1] && row[0] == row[2]
+            return true if row[0] != nil && row[0] == row[1] && row[0] == row[2]
         end
         return false
     end
@@ -61,7 +64,7 @@ class Board
     
     def full?
         @board.each do |row|
-            return false if row.include?("_")
+            return false if row.include?(nil)
         end
         return true
     end
